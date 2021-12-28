d1c4fe3a53afd2c3fdc141b97348745d5d3aacba
fabiopereirareis <espynho23@gmail.com>
2021-12-28 16:56:11 -0300

    refactor: add gitignore

--- app/Views/cliente/home.php
+++ app/Views/cliente/home.php
@@ -1,3 +1,59 @@
-<content>
-    conteudo
+<content class="container py-3">
+    <!--cards-->
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+            <div class="card h-80">
+                <img src="assets/img/pizza_01.jpg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Pizzaria Temática</h5>
+                    <p class="card-text">
+                        Para atender as necessidades de todo o público que são adeptos
+                        de uma boa massa e gostam de experimentar algo novo. Como uma
+                        boa pizzaria com tema temático, num estilo de Piratas algo
+                        totalmente diferente de uma pizzaria habitual com a decoração
+                        colonial e medieval.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="assets/img/franquia.jpg" class="card-img-top" alt="Foto de Arturo Anez no Pexels" />
+                <div class="card-body">
+                    <h5 class="card-title">Rede de Franquias</h5>
+                    <p class="card-text">
+                        Além da sede própria teremos também as redes dos franqueados,
+                        que contará com todos os requintes da sede própria. Contando
+                        com um ambiente familiar, músicas ao vivo os nossos garçons
+                        com trajes de piratas e buscando o cliente até a mesa para
+                        dançar, ambiente rústico; pizzas saborosas além de outras
+                        especiarias, uma rodada de Rum como cortesia da casa além da
+                        sua imaginação que vai fazer uma viagem aos tempos medievais
+                        dos piratas algo nunca visto antes e inimaginável uma magia
+                        pura.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-8">
+                <img src="assets/img/planos.jpg" class="card-img-top" alt="Foto de Tima Miroshnichenko no Pexels" />
+                <div class="card-body">
+                    <h5 class="card-title">Planos mensais</h5>
+                    <p class="ca0rd-text">
+                        Como grande diferencial, nossa pizzaria é pioneira no sistema
+                        de venda de pizzas sob demanda, ou seja, você assina um plano
+                        e além de comer uma boa pizza também terá bônus e descontos
+                        exclusivos. Então não fique de fora e assine o plano que
+                        melhor te agrada, quer você seja uma pessoa ou uma família, a
+                        Piratas do Karibe tem a melhor opção para você.
+                    </p>
+                </div>
+                <!-- <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div> -->
+            </div>
+        </div>
+    </div>
+    <!--cards-->
 </content>
--- app/Views/cliente/templates/header.php
+++ app/Views/cliente/templates/header.php
@@ -5,13 +5,56 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Pizzaria Piratas do Karibe</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css" />
+    <script src="assests/js/bootstrap.js"></script>
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/pricing.css" rel="stylesheet" />
 </head>
 
 <body>
     <h1><?= esc($title) ?></h1>
     <header>
-        cabeça
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none">
+            <symbol id="check" viewBox="0 0 16 16">
+                <title>Check</title>
+                <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+            </symbol>
+        </svg>
+
+        <div class="container py-3">
+            <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+                <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+                    <img src="assets/brand/logo_v1.png" alt="" width="150" height="75" class="d-inline-block align-text-top me-3 border-end border-bottom rounded" />
+                    <span class="fs-4">Piratas do Karibe</span>
+                </a>
+                <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+                    <a class="me-3 py-2 text-dark text-decoration-none" href="#">Planos</a>
+                    <a class="me-3 py-2 text-dark text-decoration-none" href="#">Empresa</a>
+                    <a class="me-3 py-2 text-dark text-decoration-none" href="#">Cadastro</a>
+                    <a class="py-2 text-dark text-decoration-none" href="#">Login</a>
+                </nav>
+            </div>
+        </div>
     </header>
